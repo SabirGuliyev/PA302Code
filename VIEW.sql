a1132c561fac09d2381834418497787ec103a5ab
SabirGuliyev <sabir.guliyev@code.edu.az>
2026-06-10 22:34:24 +0400

    trigger added

--- VIEW.sql
+++ VIEW.sql
@@ -10,8 +10,15 @@ Name NVARCHAR(50) NOT NULL,
 Surname NVARCHAR(50) DEFAULT 'XXX',
 Point INT NOT NULL CHECK(Point BETWEEN 0 AND 100),
 Age TINYINT NOT NULL CHECK(Age BETWEEN 10 AND 65),
-Email VARCHAR(256) NOT NULL UNIQUE
+Email VARCHAR(256) NOT NULL UNIQUE,
+IsDeleted BIT 
 )
+
+ALTER TABLE Students ADD CreatedAt SMALLDATETIME 
+
+ALTER TABLE Students ADD IsDeleted BIT 
+
+
 
 CREATE TABLE Grades(
 Id INT PRIMARY KEY IDENTITY,
@@ -78,3 +85,66 @@ WHERE Point=(SELECT MAX(Point)  FROM StudentsWithGrades)
 
 
 --CLUSTERED    NON-CLUSTERED  VIEW
+
+
+
+ALTER TRIGGER TriggerOnStudentsDelete
+ON Students
+AFTER DELETE
+AS
+SELECT * FROM DELETED
+
+
+
+--14-Sabir Guliyev 20 Email
+
+
+CREATE TRIGGER TriggerOnStudentsInsert
+ON Students
+AFTER INSERT
+AS
+UPDATE Students SET CreatedAt=SYSDATETIME() WHERE Id=(SELECT Id FROM INSERTED)
+GETDATE()
+
+INSERT INTO Students(Name, Surname, Email, Age, Point) VALUES('Test','Testov','aaaa@',23,88)
+
+
+
+DELETE FROM Students WHERE Id IN(6,10)
+
+
+SELECT * FROM Students WHERE IsDeleted=0
+
+
+SELECT * FROM Students WHERE IsDeleted=1
+
+
+
+
+CREATE TRIGGER StudentsSoftDelete
+ON Students
+INSTEAD OF DELETE
+AS
+DECLARE @isDeleted BIT
+DECLARE @id INT
+
+SELECT @isDeleted=IsDeleted, @id=Id FROM DELETED
+
+IF(@isDeleted = 0)
+	BEGIN
+	UPDATE Students SET IsDeleted=1 WHERE Id=@id
+	END
+ELSE
+	BEGIN
+	DELETE FROM Students WHERE Id=@id
+	END
+
+
+
+
+DELETE FROM Students WHERE Id=12
+
+
+
+T-SQL
+PL SQL
